f9f39ff3c3a3b1b0b9ba1c387523f9581da5c9df
Felipe <felipe.pucsc@gmail.com>
2019-11-04 05:10:24 -0300

    Adding Success widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:alcool_gasolina/widgets/input.widget.dart';
 import 'package:alcool_gasolina/widgets/loading-button.widget.dart';
 import 'package:alcool_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -33,6 +34,10 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Success(
+            result: "Compensa utilizar x",
+            reset: () {},
+          ),
           Input(
             ctrl: _gasCtrl,
             label: "Gasolina",
