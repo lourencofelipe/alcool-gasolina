516f4ca202fa7a9700b45950f7aadfd287adb745
Felipe <felipe.pucsc@gmail.com>
2019-11-07 23:48:54 -0300

    Adding Stateful class

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,12 +1,21 @@
 import 'package:alcool_gasolina/widgets/logo.widget.dart';
 import 'package:alcool_gasolina/widgets/submit-form.dart';
+import 'package:alcool_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
-class HomePage extends StatelessWidget {
-  var _gasCtrl =
-      new MoneyMaskedTextController(); // Create a new private money mask controller.
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _gasCtrl = new MoneyMaskedTextController();
+
   var _alcCtrl = new MoneyMaskedTextController();
+  var _busy = false;
+  var _completed = false;
+  var _resultText = "Compensa utilizar álcool";
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +25,39 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          SubmitForm(
-            alcCtrl: _alcCtrl,
-            gastCtrl: _gasCtrl,
-            submitFunc: () {},
-            busy: false,
-          ),
+          _completed
+              ? Success(
+                  result: _resultText,
+                  reset: () {},
+                )
+              : SubmitForm(
+                  alcCtrl: _alcCtrl,
+                  gastCtrl: _gasCtrl,
+                  submitFunc: calculate,
+                  busy: _busy,
+                ),
         ],
       ),
     );
+  }
+
+  Future calculate() {
+    double alc = double.parse(
+          _alcCtrl.text.replaceAll(new RegExp(r'[,.]'),
+              ''), // Replace the ',' to '.' because double does not accept ','
+        ) /
+        100;
+
+    double gas = double.parse(
+          _gasCtrl.text.replaceAll(new RegExp(r'[,.]'), ''),
+        ) /
+        100;
+
+    double res = alc / gas;
+
+    setState(() {
+      _completed = false;
+      _busy = true;
+    });
   }
 }
